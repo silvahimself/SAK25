94c5ac84709d377ba3dd30658947061b1b098720
silvahimself <david@silvahimself.com>
2025-01-08 20:08:40 +0000

    Disable Murmur

--- SAK25-console/Program.cs
+++ SAK25-console/Program.cs
@@ -1,4 +1,5 @@
-﻿using SAK25.Helpers;
+﻿//using Murr;
+using SAK25.Helpers;
 
 namespace SAK25_console
 {
@@ -6,29 +7,10 @@ namespace SAK25_console
     {
         static async Task Main(string[] args)
         {
-            var options = new FileSearchOptions
-            {
-                Extension = "cs",
-                IgnoreDirectories = ["node_modules", "Library", "Package", ".next", ".git", ".vs", ".vscode", "bin", "obj", "src"]
-            };
-
-            Console.Write("Search Path > ");
-            var path = Console.ReadLine();
-            var files = FileHelpers.GetAllFilesRecursive(path, options);
-
-            string extensions = "";
-
-
-            foreach (var file in files)
-            {
-                if (file.ToLowerInvariant().Contains("extension"))
-                {
-                    var text = await File.ReadAllTextAsync(file);
-                    extensions += $"--------------------\r\n{text}\r\n--------------------\r\n\r\n";
-                }
-            }
-
-            Console.WriteLine(extensions);
+            Console.WriteLine("File paths (comma separated):");
+            var files = Console.ReadLine();
+            //var m = new Murmur();
+            //await m.CreateMermaid(files.Split(','));
         }
     }
 }
