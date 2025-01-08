2a3775c2706d35fb58ffcf4dce287651a1c92a41
silvahimself <david@silvahimself.com>
2025-01-08 20:08:10 +0000

    Upgrade to .NET9, fix project path

--- SAK25/Helpers/EnvironmentHelpers.cs
+++ SAK25/Helpers/EnvironmentHelpers.cs
@@ -1,4 +1,6 @@
 ﻿using SAK25.Types;
+using System;
+using System.IO;
 
 namespace SAK25.Helpers;
 
@@ -9,4 +11,16 @@ public static class EnvironmentHelpers
             ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.User)
             ?? Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine)
             ?? throw Exceptions.EnvVarMissing(name);
+
+    // [PROJECT_PATH]/bin/debug/
+    public static string ExecutingDir() => Environment.CurrentDirectory;
+
+
+    // [PROJECT_PATH]/bin/
+    public static string BinDir() => Directory.GetParent(ExecutingDir()).Parent.FullName;
+
+    // [PROJECT_PATH]
+    public static string ProjectDir() => Directory.GetParent(ExecutingDir())?.Parent?.Parent?.Parent?.FullName ?? throw new Exception("Unable to read project directory, something went terribly wrong." +
+        "\n\n" +
+        $"Executing Dir: {ExecutingDir()}");
 }
